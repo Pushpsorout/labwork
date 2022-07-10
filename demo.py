c4cd0b0c16deeb0e22e97e706a265746101af0b1
Pushpender <53961784+Pushpsorout@users.noreply.github.com>
2022-07-10 21:57:41 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,1 @@
-import math
-x=float(input("enter the first side of the triangle"))
-y=float(input("enter the second side of the triangle"))
-z=float(input("enter the third sid of the triangle"))
-s=(x+y+z)/2
-d=math.sqrt(s*(s-x)*(s-y)*(s-z))
-print("area of triangle=",d)
+print("i dont know what to do")
